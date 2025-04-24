6f0ced1d97ffec695ec3b2185a648c87f187c21a
christosxbal <xrhstos.m321@gmail.com>
2025-04-24 18:42:31 +0300

    Maybe ficed an error with chart.jsx

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HourlyForecastChart from "./HourlyForeCastChart";
+import HourlyForecastChart from "./HourlyForeCastChart.jsx";
 
 const HourlyForecast = ({ data }) => {
   return (
